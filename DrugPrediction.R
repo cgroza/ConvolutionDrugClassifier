971e93dd7c8d525d6e092e7b0262354eefea6e64
Cristian Groza <grozac@ymail.com>
2017-01-02 23:33:15 -0500

    New comments and modified parameter.

--- DrugPrediction.R
+++ DrugPrediction.R
@@ -2,26 +2,33 @@ library(ChemmineR)
 library(abind)
 library(mxnet)
 
-# number coding for atoms
+# Number coding for atoms
 AtomNumber = 1:10
 names(AtomNumber) = c("H", "C", "N", "O", "S", "P", "F", "Cl", "I", "Br")
+
+## This function mines the atom data from SDF files into a 3D matrix.
+## Dimensions: drug, label + atoms, X Y Z under each atom.
 
 sdfTo3dArray = function(sdfMolecules, width, molClass)
 {
   nOfSdf = length(sdfMolecules)
+  ## Since we have empty fields, create the smallest matrix that will
+  ## accommodate the largest molecule, filling empty features with
+  ## the maximum integer. This should make the neural network consider
+  ## them as outliers.
   Matrix = array(dim=c(4, width, nOfSdf), data=.Machine$integer.max)
 
   j = 1
-  # iterate through every sdf molecule
+  # Iterate through every sdf molecule
   while (j <= nOfSdf)
   {
-    # access jth molecule
+    # Access jth molecule
     sdfMolecule = sdfMolecules[[j]]
-    # retrieve atom block that contains the atoms and their coordinates
+    # Retrieve atom block that contains the atoms and their coordinates
     moleculeAtoms = atomblock(sdfMolecule)
-    # retrieve all atoms
+    # Retrieve all atoms
     atoms = rownames(moleculeAtoms)
-    # for every atom, assign its name and coordinates in the matrix
+    # For every atom, assign its name and coordinates in the matrix
     a = 1
     while(a <= length(atoms))
     {
@@ -39,30 +46,37 @@ sdfTo3dArray = function(sdfMolecules, width, molClass)
   return(Matrix)
 }
 
-# load molecules
+# Load molecules
 stimulantsSDFs = read.SDFset("stimulants.sdf")
 sedativesSDFs = read.SDFset("sedatives.sdf")
-# width of matrix is the maximum number of atoms in a molecule plus 1 (class)
+
+# Width of matrix is the maximum number of atoms in a molecule plus 1 (class).
 arrayWidth =  max(max(sapply(atomcount(stimulantsSDFs), sum)) + 1,
                   max(sapply(atomcount(sedativesSDFs), sum)) + 1)
 
-# Load and set dummy variable according to class: Stimulant (1) and Sedative (1)
+# Load and set dummy variable according to class: Stimulant (1) and Sedative (1).
 stimulants = sdfTo3dArray(stimulantsSDFs, arrayWidth, 1)
 sedatives = sdfTo3dArray(sedativesSDFs, arrayWidth, 0)
 
-# bind the sedatives and stimulant datasets together
+# Bind the sedatives and stimulant datasets together.
 drugMatrix = abind(stimulants, sedatives, along=3)
 
-## set seed for number generator. Ensures repeatable results.
+## Set seed for number generator. Ensures repeatable results.
 set.seed(1)
 
 dataSize = length(drugMatrix[1,1,])
+
+## Separate data into training and testing subsets.
+## We will use majority of data to train neural network.
 sampleSize = 0.8 * dataSize
+
 trainingSample = sample(dataSize, sampleSize)
 testingSample = setdiff(1:dataSize, trainingSample)
 
 trainingSet = drugMatrix[,, trainingSample]
 testingSet = drugMatrix[,,testingSample]
+
+## Separate labels of drugs from their SDF input for both testing and training.
 
 trainingX = trainingSet[,-1,]
 trainingY = trainingSet[1,1,]
@@ -70,6 +84,7 @@ trainingY = trainingSet[1,1,]
 testingX = testingSet[,-1,]
 testingY = testingSet[1,1,]
 
+## mxnet requires a 4D input array?
 dim(trainingX) = c(4, 59, 1, 98)
 dim(testingX) = c(4, 59, 1, 25)
 
@@ -77,6 +92,13 @@ data <- mx.symbol.Variable('data')
 # 1st convolutional layer
 conv_1 <- mx.symbol.Convolution(data = data, kernel = c(1, 1), num_filter = 20)
 tanh_1 <- mx.symbol.Activation(data = conv_1, act_type = "tanh")
+
+## NOTE: When pooing kernel size is small (1,1), accuracy is improved (96%).
+## During training, accuracy peaks at 89%.
+## Prevents overfitting and forces generalizations? When a kernel size of (2,2)
+## is used, accuracy drops to 76%. However, during training, accuracy peaks at 100%.
+## Is this evidence of overfitting?
+
 pool_1 <- mx.symbol.Pooling(data = tanh_1, pool_type = "max", kernel = c(1, 1), stride = c(2, 2))
 # 2nd convolutional layer
 conv_2 <- mx.symbol.Convolution(data = pool_1, kernel = c(1, 1), num_filter = 50)
@@ -94,20 +116,24 @@ NN_model <- mx.symbol.SoftmaxOutput(data = fc_2)
 
 devices <- mx.cpu()
 
-# Train the model
+# Train the model.
 model <- mx.model.FeedForward.create(NN_model,
                                      X = trainingX,
                                      y = trainingY,
                                      ctx = devices,
                                      num.round = 300,
-                                     array.batch.size = 40,
+                                     array.batch.size = length(trainingY),
                                      learning.rate = 0.01,
                                      momentum = 0.9,
                                      eval.metric = mx.metric.accuracy,
                                      epoch.end.callback = mx.callback.log.train.metric(100))
 
+## Convert output probability matrix for each feature into list of labeled data.
+
 predictProbs <- predict(model, testingX)
 predictedLabels <- max.col(t(predictProbs)) - 1
 
+## Evaluate accuracy from a confusion table.
 confusionTable = table(predictedLabels, testingY)
 accuracy = sum(diag(confusionTable))/25
+accuracy
